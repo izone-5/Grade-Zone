7012126f0e51ff518a59b317430aac2b7b049b3c
Gaudencia <conny@students.uonbi.ac.ke>
2023-02-22 14:35:14 +0300

    initial commit

---
+++ grade-zone/src/pages/Nopage.js
@@ -0,0 +1,13 @@
+import './Nopage.scss';
+
+
+const NoPage = () => {
+  
+    return (
+      <div className='nopage'>
+        <h1>404 Page Not Found!!!</h1>
+      </div>
+    );
+  };
+  
+  export default NoPage;
