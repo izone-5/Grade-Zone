d7bce34109aa88996952d698ef0f674bc6479f37
Gaudencia <conny@students.uonbi.ac.ke>
2023-02-27 11:19:21 +0300

    initial commit

--- grade-zone/src/pages/Authentication/Authentication.js
+++ grade-zone/src/pages/Authentication/Authentication.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { Outlet,useLocation } from "react-router-dom";
+import user from '../../assets/user.png';
+import './Authentication.scss'
 
 const Authentication = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+ const location = useLocation()
+    const {pathname}= location
+    let slitPath=pathname.split('/')[1]
+    return(
+        <>
+      <div className='loginWrapper'>
+         <div className='loginImageWrapper'>
+          <img className='loginImg' src={user}/>
+         </div>
+         <Outlet/>
+      </div>
+      <div>
+        <button className='loginBtn'>continue</button>
+      </div>
+      <div className='logintext'>
+       {
+        slitPath==='in'? <p>Don't have an account? <span><a href="up">sign up</a></span></p>:
+        <p>Already have an account? <span><a href="in">sign in</a></span></p>
+       }
+      </div>
+    </>
+    )
 }
 
 export default Authentication
---
+++ grade-zone/src/redux/authSlice.js
@@ -0,0 +1,46 @@
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+
+import axios from "axios";
+import '../mockApi/MockApi'
+
+export const getProfileData = createAsyncThunk(
+    'auth/getProfileDateById',
+     async (arg, {rejectWithValue}) => {
+    try {
+        const data = await axios.get(`/users/${arg}`)
+        return data
+    } catch (error) {
+        rejectWithValue(error.response.data)
+    }
+})
+const initialState={
+    profileData: {},
+        access: false,
+        loading: false,
+        message: "",
+        isSuccess: false
+}
+export const authSlice=createSlice({
+    name:"auth",
+    initialState,
+    reducers:{},
+    extraReducers:(builder)=>{
+         builder.addCase(getProfileData.pending, (state) =>{
+            state.loading = true;
+        })
+
+        builder.addCase(getProfileData.fulfilled, (state, {payload}) =>{
+            state.loading = false;
+            state.profileData = payload;
+            state.isSuccess = true;
+        })
+
+        builder.addCase(getProfileData.rejected, (state, {payload}) =>{
+            state.loading = false;
+            state.message = payload;
+            state.isSuccess = false;
+        })
+    }
+});
+export const {}=authSlice.actions;
+export default authSlice.reducer
